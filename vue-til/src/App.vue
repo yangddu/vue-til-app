28794acb49507711205e0e98e028eb9642b35043
jooyoung <jooyoung@pood.pet>
2022-08-15 21:17:17 +0900

    add: history mode, router처리

--- vue-til/src/App.vue
+++ vue-til/src/App.vue
@@ -3,6 +3,7 @@
     <header>
       <router-link to="/login">로그인</router-link>
       <router-link to="/signup">회원가입</router-link>
+      <router-view></router-view>
     </header>
   </div>
 </template>
---
+++ vue-til/src/views/NotFoundPage.vue
@@ -0,0 +1,9 @@
+<template lang="">
+  <div>Page is not found</div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang=""></style>

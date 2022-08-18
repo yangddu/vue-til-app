710f14ac66d293a7b52d223f65b79450f50c65bf
jooyoung <jooyoung@pood.pet>
2022-08-18 09:53:25 +0900

    add: 회원가입 폼 마크업

---
+++ vue-til/src/components/SignupForm.vue
@@ -0,0 +1,21 @@
+<template lang="">
+  <form>
+    <div>
+      <label for="username">id: </label>
+      <input id="username" type="text" />
+    </div>
+    <div>
+      <label for="password">pw: </label>
+      <input id="password" type="text" />
+    </div>
+    <div>
+      <label for="nickname">nickname: </label>
+      <input id="nickname" type="text" />
+    </div>
+    <button typee="submit">login</button>
+  </form>
+</template>
+<script>
+export default {};
+</script>
+<style lang=""></style>
--- vue-til/src/views/SignupPage.vue
+++ vue-til/src/views/SignupPage.vue
@@ -1,9 +1,15 @@
 <template lang="">
-  <div>signup</div>
+  <SignupForm></SignupForm>
 </template>
 
 <script>
-export default {};
+import SignupForm from '@/components/SignupForm.vue';
+
+export default {
+  components: {
+    SignupForm,
+  },
+};
 </script>
 
 <style lang=""></style>
